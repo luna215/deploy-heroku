78238a4b1082e4685f4fa2205257094b7f2b340d
luna215 <paulluna0215@gmail.com>
2020-02-18 14:55:53 -0800

    Added first test

---
+++ test/index.spec.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const server = require('../app.js');
+
+describe('test', () => {
+    it('should return a string', () => {
+        expect('hello world').to.equal('hello world');
+    });
+});
