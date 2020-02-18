a67dfc34d478127d85114902bc2bb5304ce9c9b1
luna215 <paulluna0215@gmail.com>
2020-02-18 14:39:20 -0800

    installed dependencies to work with jenkins CI

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## Update
 This was a success and this simple app is hosted here:
 ```https://deploy-heroky.herokuapp.com/```
+
+## Summary
+This started as a simple project to automate deployments to heroku, but now I will also use it for starting simple Jenkins CI tests that will automatically deploy to heroku if all tests pass.
--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+app.get('/', function(req, res) {
+  res.send('hello world');
 });
 
-server.listen(port, () => {
-  console.log(`Server running at port `+port);
-});
+app.listen(process.env.port || 3000);
+
+module.exports = app;
