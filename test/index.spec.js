0af3edb6004079c810f4b7fdbea27a8eddb8c58c
luna215 <paulluna0215@gmail.com>
2020-02-19 07:20:10 -0800

    testing deployment, again

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,9 @@
 const expect = require('chai').expect;
 const server = require('../app.js');
 
-// describe('test', () => {
-//     it('should return a string', () => {
-//         expect('hello world').to.equal('hello world');
-//     });
-// });
+describe('test', () => {
+    it('should return a string', () => {
+        expect('hello world').to.equal('hello world');
+    });
+});
 
